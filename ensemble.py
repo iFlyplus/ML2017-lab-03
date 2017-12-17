a8b5dc6367783a4b5fa5bc90a983922cb44608cd
ziwei <iFly_2015@outlook.com>
2017-12-17 15:37:52 +0800

    update code 2017.12.17

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,14 @@
+#
+# Authors: Ziwei Zhang
+# Date: Dec. 16, 2017
+#
+# Machine Learning 2017 Experiments No.3
+#    Face Classification Based on AdaBoost Algorithm
+#    Part 2: AdaBoostClassifier
+#
 import pickle
+import copy
+import numpy as np
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,20 +20,122 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        self.weak_classifiers = []
+        self.weak_classifier_weights = np.zeros(self.n_weakers_limit, dtype=np.float64)
+        self.weak_classifier_errors = np.ones(self.n_weakers_limit, dtype=np.float64)
 
     def is_good_enough(self):
         '''Optional'''
         pass
 
-    def fit(self,X,y):
+    def fit(self,X,y,verbose=True):
         '''Build a boosted classifier from the training set (X, y).
 
         Returns:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        
+        num_train = X.shape[0]
+
+        # initialize sample weight to 1.0/num_sample
+        sample_weight = np.ones((num_train,)) / num_train
+
+        # Clear any previous fit results
+        self.weak_classifiers = []
+        self.weak_classifier_weights = np.zeros(self.n_weakers_limit, dtype=np.float64)
+        self. weak_classifier_errors = np.ones(self.n_weakers_limit, dtype=np.float64)
+
+        for iboost in range(self.n_weakers_limit):
+            # boost step
+            sample_weight, classifier_weight, classifier_error = \
+                                            self._boost(iboost, X, y, sample_weight)
+            
+            # early terminite
+            if sample_weight is None:
+                if verbose:
+                    print('the error of current classifier as bad as random guessing(or worse), stop boost')
+                break
+            
+            self.weak_classifier_weights[iboost] = classifier_weight
+            self.weak_classifier_errors[iboost] = classifier_error
+            
+            # stop is error is 0
+            if classifier_weight == 0:
+                if verbose:
+                    print('the error of current classifier is 0, stop boost')
+                break
+
+            if verbose:
+                print('boost step %d / %d: classifier weight: %.3f, classifier error: %.3f'
+                        % (iboost+1, self.n_weakers_limit, classifier_weight, classifier_error))
+            
+
+
+
+    def _boost(self, iboost, X, y, sample_weight):
+        '''
+        Perform a single boost.
+        Notice: the boost implement here is only suitable for 
+                BINARY classification problem.
+
+        Inputs:
+        - iboost: (int) The index of the current boost iteration
+        - X: A numpy array of shape (N, D), 
+             containing training data;
+             there are N training samples each of dimension D.
+        - y: A numpy array of shape (N,) 
+             containing training labels
+        - sample_weight: A numpy array of shape (N,),
+                         containing the current sample weights. 
+
+        Returns:
+        - sample_weight: A numpy array of shape (N,) 
+                         containing the reweighted sample weights.
+                         None indicates the boosting has terminated early.
+        - classifier_weight: (float) The weight of the current boost.
+                              None indicates the boosting has terminated early.
+        - classifier_error: (float) The classification error of the current boost.
+                            None indicates the boosting has terminated early.
+        
+        '''
+        # create and train a new weak classifier
+        clf = self._create_classifier()
+        clf.fit(X, y, sample_weight=sample_weight)
+        
+        y_pred = clf.predict(X)
+        
+        incorrect = (y_pred != y)
+
+        classifier_error = np.average(incorrect, weights=sample_weight)
+
+        # stop if classifier is perfect
+        if classifier_error == 0:
+            return sample_weight, 1.0, 0.0
+        
+        # stop if classifier perform badly
+        if classifier_error >= 0.5:
+            self.classifiers.pop(-1)
+            if len(self.classifiers) == 0:
+                raise ValueError('weak_classifier provided can not used to create an emsemble classifier')
+            return None, None, None
+
+        classifier_weight = 0.5 * np.log((1.0 - classifier_error) / classifier_error)
+
+        zm = np.sum(sample_weight * np.exp(-classifier_weight * y * y_pred))
+        sample_weight *= np.exp(-classifier_weight * y * y_pred)
+        sample_weight /= zm
+
+        return sample_weight, classifier_weight, classifier_error
+    
+    def _create_classifier(self):
+        '''create a copy of self.weak_classifier'''
+        clf = copy.deepcopy(self.weak_classifier)
+        self.weak_classifiers.append(clf)
+        return clf
+
 
 
     def predict_scores(self, X):
@@ -35,7 +147,8 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        scores = sum((clf.predict(X) * w for clf, w in zip(self.weak_classifiers , self.weak_classifier_weights)))
+        return scores
 
     def predict(self, X, threshold=0):
         '''Predict the catagories for geven samples.
@@ -46,8 +159,41 @@ class AdaBoostClassifier:
 
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
+            shape (N,)
         '''
-        pass
+        scores = sum((clf.predict(X) * w for clf, w in zip(self.weak_classifiers, self.weak_classifier_weights)))
+        # y_pred /= X.shape[0]
+        y_pred = np.ones((X.shape[0],))
+        y_pred[scores < threshold] = -1
+        return y_pred
+    
+    def staged_predict(self, X, threshold=0):
+        '''
+        Yeild the ensemble predicrtion after after each boost.
+
+        Inputs:
+        - X: A numpy array of shape (N, D), 
+             containing training data;
+             there are N training samples each of dimension D.
+        - threshold: The demarcation number of deviding the samples into two parts.
+
+        Returns:
+        - y: A generator of array of shape(N,), 
+             containing prediction of each sample in X.
+        '''
+        scores = None
+        for clf, w in zip(self.weak_classifiers, self.weak_classifier_weights):
+            current_score = clf.predict(X) * w # (N,)
+
+            if scores is None:
+                scores = current_score
+            else:
+                scores += current_score
+
+            current_pred = np.ones((X.shape[0],))
+            current_pred[scores < threshold] = -1
+            yield current_pred
+
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,189 @@
+#
+# Authors: Ziwei Zhang
+# Date: Dec. 16, 2017
+#
+# Machine Learning 2017 Experiments No.3
+#    Face Classification Based on AdaBoost Algorithm
+#    Part 1: train script
+#
+import os
+import numpy as np
+from scipy.misc import imread, imsave, imresize
+from six.moves import cPickle as pickle
+import matplotlib.pyplot as plt
+from feature import NPDFeature
+from sklearn.tree import DecisionTreeClassifier
+from ensemble import AdaBoostClassifier
 
+def get_data(num_train=600):
+    data_folder = './datasets/original/'
+    if os.path.exists(os.path.join(data_folder, 'dataset.pickle')):
+        print('Use pickled dataset.')
+        dataset = load(data_folder + 'dataset.pickle')
+    else:
+        print('Load raw image.')
+        dataset = {}
+        faceset = load_data(os.path.join(data_folder, 'face'))
+        nonfaceset = load_data(os.path.join(data_folder, 'nonface'))
+        dataset['face'] = preprocess_data(faceset)
+        dataset['nonface'] = preprocess_data(nonfaceset)
+        # pickle dataset for later use
+        save(dataset, data_folder + 'dataset.pickle')
+    
+    # num_train + num_val = total sample number
+    # num_train_face + num_train_nonface = num_train
+    # num_train_face + num_val_face = total face sample number
+    # num_val_nonface = num_val - num_val_face
+    #                 = total - num_train - (total_face - num_train_face)
+    #                 = total_non_face - num_train_nonface
+    # ==> num_train_nonface + num_val_nonface = total non face sample number
+    num_feature = dataset['face'].shape[1]
+    num_val = dataset['face'].shape[0] + dataset['nonface'].shape[0] - num_train
+    num_train_face = num_train // 2
+    num_train_nonface = num_train - num_train_face
+    num_val_face = dataset['face'].shape[0] - num_train_face
+    num_val_nonface = num_val - num_train_face
+
+    X_train = np.ndarray(shape=(num_train, num_feature), dtype=np.float32)
+    y_train = np.ones((num_train,))
+    X_test = np.ndarray(shape=(num_val, num_feature), dtype=np.float32)
+    y_test = np.ones((num_val,))
+
+    X_train[:num_train_face] = dataset['face'][:num_train_face]
+    X_train[num_train_face:] = dataset['nonface'][:num_train_nonface]
+    y_train[num_train_face:] = -1
+    X_test[:num_val_face] = dataset['face'][num_train_face:]
+    X_test[num_val_face:] = dataset['nonface'][num_train_nonface:]
+    y_test[num_val_face:] = -1
+
+    return {
+      'X_train': X_train, 'y_train': y_train,
+      'X_test': X_test, 'y_test': y_test
+    }
+
+def load_data(data_folder):
+    '''Load image data from data_folder'''
+    image_files = os.listdir(data_folder)
+    imageset = None
+    image_index = 0
+    for image in image_files:
+        image_file = os.path.join(data_folder, image)
+        try:
+            img = imread(image_file).astype(float)
+            '''
+            if img.shape != (image_size, image_size, 3):
+                raise Exception('Unexpected image shape: %s' % str(img.shape))
+            '''
+            if imageset is None:
+                image_size = img.shape[0]
+                imageset = np.ndarray(shape=(len(image_files), image_size,image_size, 3), 
+                                    dtype=np.float32)
+            imageset[image_index] = img
+            image_index += 1
+        except IOError as e:
+            print('Could not read:', image_file, ':', e)
+    num_image = image_index
+    imageset = imageset[:num_image, :, :, :]
+
+    return imageset
+
+def preprocess_data(dataset):
+    '''convert image to greyscale,resize to (24,24) and extract NPD feature'''
+    # feature size: 165600
+    num_feature = 165600
+    num_sample = dataset.shape[0]
+    dataset_processed = np.ndarray(shape=(num_sample, num_feature), 
+                                    dtype=np.float32)
+    for i in range(num_sample):
+        img_grey = convert_to_grey(dataset[i])
+        img_grey_resize = imresize(img_grey, (24,24))
+        npdfeature = NPDFeature(img_grey_resize).extract()
+        dataset_processed[i] = npdfeature
+    return dataset_processed
+
+def convert_to_grey(img):
+    return img[:,:,0] * 0.3 + img[:,:,1] * 0.59 + img[:,:,2] * 0.11
+
+
+def save(file, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(file, f)
+
+def load(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+def compute_accuracy(y_pred, y_grondtrue):
+    return np.mean(np.array(y_pred == y_grondtrue), dtype = np.float32)
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    # load dataset. (total 1000 samples, use num_train samples to fit adaboost model)
+    num_train = 600
+    dataset = get_data(num_train=num_train)
+    X_train, y_train, X_test, y_test = dataset['X_train'], dataset['y_train'], dataset['X_test'], dataset['y_test']
+    print('datasize: 1000, use %d samples to fit adaboost model.' % num_train)
+    # setup model parameter.
+    #       DecisionTreeClassifier
+    #           max_depth: The maximum depth of the tree.
+    #           min_samples_leaf:
+    #           min_samples_split: The minimum number of samples required to split an internal node
+    #       AdaBoostClassifier:
+    #           weak_classifier: default use DecisionTreeClassifier
+    #           n_weakers_limit: maximum number of weak classifier to boost an adaboost model.
+    max_depth = 1
+    min_samples_leaf = 1
+    min_samples_split = None
+    n_weakers_limit = 25
 
+    print('\nUse DecisionTreeClassifier as weak classifiers.')
+    print('\nTree settings:\n- max_depth = %d\n- min_samples_leaf = %d\n- min_samples_split = %s'
+           % (max_depth, min_samples_leaf, str(min_samples_split)))
+    print('\nAdaboost model settings:\n- n_weakers_limit = %d' % n_weakers_limit)
+    print('\nStart boosting the model:')
+
+    dt = DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+    ada_model = AdaBoostClassifier(weak_classifier=dt, n_weakers_limit=n_weakers_limit)
+    ada_model.fit(X_train, y_train,verbose=True)
+    
+    # predict labels for X_train and X_test and compute accuracy.
+    y_pred_train = ada_model.predict(X_train)
+    y_pred_val = ada_model.predict(X_test)
+    train_accuracy = compute_accuracy(y_pred_train, y_train)
+    val_accuracy = compute_accuracy(y_pred_val, y_test)
+
+    print('\ntraining accuracy: %.3f.\nvaluation accuracy: %.3f.' % (train_accuracy, val_accuracy))
+    
+    # visualize result.
+    # 
+    # plot the ensemble prediction accuracy after each boost
+    #
+    print('\nVisualize result.')
+    ada_err_train = np.zeros((n_weakers_limit,))
+    for i, y_pred in enumerate(ada_model.staged_predict(X_train)):
+        ada_err_train[i] = compute_accuracy(y_pred, y_train)
+
+    ada_err_val = np.zeros((n_weakers_limit,))
+    for i, y_pred in enumerate(ada_model.staged_predict(X_test)):
+        ada_err_val[i] = compute_accuracy(y_pred, y_test)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.plot(np.arange(n_weakers_limit) + 1, ada_err_train,
+            label='AdaBoost Train Error',
+            color='blue')
+
+    ax.plot(np.arange(n_weakers_limit) + 1, ada_err_val,
+            label='AdaBoost Test Error',
+            color='red')
+    
+    ax.set_xlabel('n_classifiers')
+    ax.set_ylabel('accuracy')
+
+    leg = ax.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.7)
+
+    plt.show()
+
+# deep=1, n=20, ===> train_a=1.0, test_a=0.967
+# deep=1, n=10, ===> train_a=0.993, test_a=0.942
